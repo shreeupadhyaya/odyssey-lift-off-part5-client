61da154591f1e2fe5848469ce603e5e0c89ac2e8
Shree <shree912@yahoo.com>
2022-12-23 20:18:58 -0800

    change server endpoint

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import GlobalStyles from "./styles";
 import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-part5-server-production-33ed.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
@@ -18,3 +19,7 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById("root")
 );
+/*
+APOLLO_KEY=service:OdysseyTutorial-6ra4ju:TvqHlY3UVgWmz89liWTy8A
+APOLLO_GRAPH_REF=OdysseyTutorial-6ra4ju@current
+APOLLO_SCHEMA_REPORTING=true*/
